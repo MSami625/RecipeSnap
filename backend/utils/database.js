77aade8623b46668f43584c28353bdb825b032e7
M Sami <beboolean@gmail.com>
2024-10-04 00:05:32 +0530

    edit: setup proj for deploying

--- backend/utils/database.js
+++ backend/utils/database.js
@@ -1,13 +1,13 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize(
-  `${process.env.DB_NAME}`,
-  `${process.env.DB_USERNAME}`,
-  `${process.env.DB_PASS}`,
-  {
-    dialect: "mysql",
-    host: `${process.env.DB_HOST}`,
-  }
-);
+const sequelize = new Sequelize(process.env.DB_CONNECTION, {
+  dialect: "mysql",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 module.exports = sequelize;

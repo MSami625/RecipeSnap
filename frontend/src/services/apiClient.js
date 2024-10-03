c7125649130e08fe878bc9611f4b856b59f78171
M Sami <beboolean@gmail.com>
2024-10-04 00:22:40 +0530

    edit: API endpoint

--- frontend/src/services/apiClient.js
+++ frontend/src/services/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://recipe-snap-api.vercel.app/api",
 });
 
 export default apiClient;
